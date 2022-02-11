2fa49a41bc6b1e85f444036d464bc2a0b4b59cce
qiubin <qiub@hotmail.com>
2022-02-11 11:18:32 +0800

    增加 默认同时只能开一套程序

--- BossKey/Program.cs
+++ BossKey/Program.cs
@@ -1,22 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace BossKey
 {
     internal static class Program
     {
+
+
+        public static Process RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            foreach (Process process in processes)
+            {
+                if (process.Id != current.Id)
+                {
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == current.MainModule.FileName)
+                    {
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static void HandleRunningInstance(Process Instance)
+        {
+            //ShowWindowAsync(Instance.MainWindowHandle, 3);
+            //SetForegroundWindow(Instance.MainWindowHandle);
+        }
+
+
         /// <summary>
         /// 应用程序的主入口点。
+        /// 默认同时只能开一套程序
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm(args));
+            Process Instance = RunningInstance();
+            if (Instance == null)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm(args));
+            }
+            else
+            {
+                HandleRunningInstance(Instance);
+            }
         }
     }
 }
